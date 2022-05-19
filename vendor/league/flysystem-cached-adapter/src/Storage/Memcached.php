8b9c15542df65aa9ec134c94d14da3c9ff3fa148
喵千寻 <101342651+MoeCinnamo@users.noreply.github.com>
2022-05-19 10:40:06 +0800

    Create Memcached.php

---
+++ vendor/league/flysystem-cached-adapter/src/Storage/Memcached.php
@@ -0,0 +1,28 @@
+<?php
+namespace League\Flysystem\Cached\Storage;
+use Memcached as NativeMemcached;
+class Memcached extends AbstractCache
+{
+    protected $key;
+    protected $expire;
+    protected $memcached;
+    public function __construct(NativeMemcached $memcached, $key = 'flysystem', $expire = null)
+    {
+        $this->key = $key;
+        $this->expire = $expire;
+        $this->memcached = $memcached;
+    }
+    public function load()
+    {
+        $contents = $this->memcached->get($this->key);
+        if ($contents !== false) {
+            $this->setFromStorage($contents);
+        }
+    }
+    public function save()
+    {
+        $contents = $this->getForStorage();
+        $expiration = $this->expire === null ? 0 : time() + $this->expire;
+        $this->memcached->set($this->key, $contents, $expiration);
+    }
+}
