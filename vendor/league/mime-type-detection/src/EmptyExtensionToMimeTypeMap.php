1c25adfb8cd31515655e231bc010815669b313ad
喵千寻 <101342651+MoeCinnamo@users.noreply.github.com>
2022-05-19 13:27:16 +0800

    Create EmptyExtensionToMimeTypeMap.php

---
+++ vendor/league/mime-type-detection/src/EmptyExtensionToMimeTypeMap.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+namespace League\MimeTypeDetection;
+class EmptyExtensionToMimeTypeMap implements ExtensionToMimeTypeMap
+{
+    public function lookupMimeType(string $extension): ?string
+    {
+        return null;
+    }
+}
