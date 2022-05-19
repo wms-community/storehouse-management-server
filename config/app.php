5a2ecb5a2bfc5209a30d08c38fc48ba56a6b24d5
喵千寻 <101342651+MoeCinnamo@users.noreply.github.com>
2022-05-19 20:03:04 +0800

    Update app.php

--- config/app.php
+++ config/app.php
@@ -1,4 +1,5 @@
 <?php
+
 return [
     /*
     |--------------------------------------------------------------------------
@@ -8,7 +9,59 @@ return [
     | Version of Storehouse Management Server.
     |
     */
-    'version' => '6.0.0-rc.2',
+    'version' => '0.0.1-alpha-202205190757',
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Update Source
+    |--------------------------------------------------------------------------
+    |
+    | Where to get information of new versions.
+    |
+    */
+    'update_source' => env(
+        'UPDATE_SOURCE',
+        'Comming soon...'
+    ),
+    
+    'name' => env('APP_NAME', 'storehouse_management'),
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Application Environment
+    |--------------------------------------------------------------------------
+    |
+    | This value determines the "environment" your application is currently
+    | running in. This may determine how you prefer to configure various
+    | services the application utilizes. Set this in your ".env" file.
+    |
+    */
+    'env' => env('APP_ENV', 'production'),
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Application Debug Mode
+    |--------------------------------------------------------------------------
+    |
+    | When your application is in debug mode, detailed error messages with
+    | stack traces will be shown on every error that occurs within your
+    | application. If disabled, a simple generic error page is shown.
+    |
+    */
+    'debug' => (bool) env('APP_DEBUG', false),
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Application Timezone
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the default timezone for your application, which
+    | will be used by the PHP date and date-time functions. We have gone
+    | ahead and set this to a sensible default for you out of the box.
+    |
+    */
+    'timezone' => env('Asia/Shanghai'),
+    
     'app_host'         => env('app.host', ''),
     'app_namespace'    => '',
     'with_route'       => true,
