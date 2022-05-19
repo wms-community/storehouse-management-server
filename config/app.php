dd53ed63bc247cbd30a7cc7d2c364ea1fbe9cf37
喵千寻 <101342651+MoeCinnamo@users.noreply.github.com>
2022-05-19 20:06:46 +0800

    Update app.php

--- config/app.php
+++ config/app.php
@@ -10,6 +10,20 @@ return [
     */
     'version' => '0.0.1-alpha-202205190805',
     
+    /*
+    |--------------------------------------------------------------------------
+    | Update Source
+    |--------------------------------------------------------------------------
+    |
+    | Where to get information of new versions.
+    |
+    */
+    'update_source' => env(
+        'UPDATE_SOURCE',
+        'https://dev.azure.com/blessing-skin/51010f6d-9f99-40f1-a262-0a67f788df32/_apis/git/'.
+        'repositories/a9ff8df7-6dc3-4ff8-bb22-4871d3a43936/Items?path=%2Fupdate.json'
+    ),
+    
     'app_host'         => env('app.host', ''),
     'app_namespace'    => '',
     'with_route'       => true,
