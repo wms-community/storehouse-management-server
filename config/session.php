8a0dbd2eb9924eda7028dc87813d7ffe7de6da40
喵千寻 <101342651+MoeCinnamo@users.noreply.github.com>
2022-05-20 14:17:17 +0800

    Update session.php

--- config/session.php
+++ config/session.php
@@ -8,12 +8,45 @@ return [
     'name'           => 'PHPSESSID',
     // SESSION_ ID submission variable to solve the cross domain problem of flash upload
     'var_session_id' => '',
-    // The drive mode supports file cache
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Default Session Driver
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the default session "driver" that will be used on
+    | requests. By default, we will use the lightweight native driver but
+    | you may specify any of the other wonderful drivers provided here.
+    |
+    | Supported: "file", "cache", 
+    |
+    */
     'type'           => 'file',
-    // The storage connection ID is valid when type uses cache
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Session Cache Store
+    |--------------------------------------------------------------------------
+    |
+    | When using the "apc", "memcached", or "dynamodb" session drivers you may
+    | list a cache store that should be used for these sessions. This value
+    | must match with one of the application's configured cache "stores".
+    |
+    */
     'store'          => null,
-    // Expiration time
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Session Lifetime
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the number of minutes that you wish the session
+    | to be allowed to remain idle before it expires. If you want them
+    | to immediately expire on the browser closing, set that option.
+    |
+    */
     'expire'         => 1440,
+    
     // prefix
     'prefix'         => '',
 ];
