87716260ea3f2a60be7e247cf6fd189d7ff69460
喵千寻 <101342651+MoeCinnamo@users.noreply.github.com>
2022-05-20 15:01:27 +0800

    Create trace.php

---
+++ config/trace.php
@@ -0,0 +1,15 @@
+<?php
+/*
+    |--------------------------------------------------------------------------
+    | Trace settings
+    |--------------------------------------------------------------------------
+    |
+    | Valid after debugging mode is turned on
+    |
+    */
+return [
+    // Built in HTML and console support extension
+    'type'    => 'Html',
+    // Read log channel name
+    'channel' => '',
+];
