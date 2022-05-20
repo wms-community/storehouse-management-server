bbb7b8fd5e5084f1e005567544a027511d5bf3be
喵千寻 <101342651+MoeCinnamo@users.noreply.github.com>
2022-05-20 14:04:54 +0800

    Create middleware.php

---
+++ config/middleware.php
@@ -0,0 +1,8 @@
+<?php
+// Middleware configuration
+return [
+    // Alias or group
+    'alias'    => [],
+    // Priority setting, the middleware in this array will be executed first according to the order in the array
+    'priority' => [],
+];
