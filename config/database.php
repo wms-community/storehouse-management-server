bd9e4ad17a891ed872618b952e4bd269e845bd6a
喵千寻 <101342651+MoeCinnamo@users.noreply.github.com>
2022-05-20 13:33:52 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -84,7 +84,7 @@ return [
         
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'blessing_skin'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'storehouse_management'), '_').'_database_'),
         ],
 
         'default' => [
