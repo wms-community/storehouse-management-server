59fe3313a43437e3cd79d0815a792bb8ca373192
喵千寻 <101342651+MoeCinnamo@users.noreply.github.com>
2022-05-20 15:13:34 +0800

    Create AppService.php

---
+++ app/AppService.php
@@ -0,0 +1,22 @@
+<?php
+declare (strict_types = 1);
+
+namespace app;
+
+use think\Service;
+
+/**
+ * Application service class
+ */
+class AppService extends Service
+{
+    public function register()
+    {
+        // Service registration
+    }
+
+    public function boot()
+    {
+        // Service startup
+    }
+}
