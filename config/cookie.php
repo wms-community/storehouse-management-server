ef0d192435714f061631c2d4edd1076c5ba25620
喵千寻 <101342651+MoeCinnamo@users.noreply.github.com>
2022-05-20 13:27:12 +0800

    Create cookie.php

---
+++ config/cookie.php
@@ -0,0 +1,20 @@
+<?php
+// +----------------------------------------------------------------------
+// | Cookie settings
+// +----------------------------------------------------------------------
+return [
+    // Cookie save time
+    'expire'    => 0,
+    // Cookie save path
+    'path'      => '/',
+    // Cookie valid domain name
+    'domain'    => '',
+    // Cookies enable secure transport
+    'secure'    => false,
+    // Httponly settings
+    'httponly'  => false,
+    // Whether to use setcookie
+    'setcookie' => true,
+    // Samesite settings, support 'strict' 'lax'
+    'samesite'  => '',
+];
