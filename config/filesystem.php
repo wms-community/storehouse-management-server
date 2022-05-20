9c66bde37208191889c289ca31141ad8ffd5e2ab
喵千寻 <101342651+MoeCinnamo@users.noreply.github.com>
2022-05-20 13:52:47 +0800

    Create filesystem.php

---
+++ config/filesystem.php
@@ -0,0 +1,56 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default Filesystem Disk
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify the default filesystem disk that should be used
+    | by the framework. The "local" disk, as well as a variety of cloud
+    | based disks are available to your application. Just store away!
+    |
+    */
+    'default' => env('filesystem.driver', 'local'),
+  
+  /*
+    |--------------------------------------------------------------------------
+    | Default Cloud Filesystem Disk
+    |--------------------------------------------------------------------------
+    |
+    | Many applications store files both locally and in the cloud. For this
+    | reason, you may specify a default "cloud" driver here. This driver
+    | will be bound as the Cloud disk implementation in the container.
+    |
+    */
+    'cloud' => env('FILESYSTEM_CLOUD', 's3'),
+  
+    /*
+    |--------------------------------------------------------------------------
+    | Filesystem Disks
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure as many filesystem "disks" as you wish, and you
+    | may even configure multiple disks of the same driver. Defaults have
+    | been setup for each driver as an example of the required options.
+    |
+    | Supported Drivers: "local", "ftp", "sftp", "s3"
+    |
+    */
+    'disks'   => [
+        'local'  => [
+            'type' => 'local',
+            'root' => app()->getRuntimePath() . 'storage',
+        ],
+        'public' => [
+            'type'       => 'local',
+            'root'       => app()->getRootPath() . '/storage',
+            'url'        => '/storage',
+            'visibility' => 'public',
+        ],
+        'testing' => [
+            'driver' => 'memory',
+        ],
+        // More disk configuration information
+    ],
+];
